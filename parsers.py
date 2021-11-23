652ac1afd1ae438d5bc67a9f7901f79f927d4e31
kuj1 <the.same.dentist@gmail.com>
2021-11-23 06:49:22 +0300

    style and refactor

--- parsers.py
+++ parsers.py
@@ -1,9 +1,6 @@
 import sqlite3
-from random import randint
 import requests
 from bs4 import BeautifulSoup
-
-# URL = 'https://www.olx.ua/zhivotnye/sobaki/ter/'
 
 HEADERS = {
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8'
